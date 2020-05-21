c0d27b59862192250dbcc6bec9b76e00c413e9e8
lnzu <2464869577@qq.com>
2020-05-21 20:11:03 +0800

    update

--- js/main.js
+++ js/main.js
@@ -25,3 +25,45 @@ $(function () {
   
   
 });
+
+class CaculatorDate {
+
+  constructor(start,end){
+    this.start = start;
+    this.end = end;
+    this.date1 = new Date(this.start);  //开始时间
+    this.date2 = new Date(this.end);    //结束时间
+  }
+  
+  getTimes() {
+    return this.date2.getTime() - this.date1.getTime();
+  }
+  
+  getDays() {
+    return Math.floor(this.getTimes() / (24 * 3600 * 1000));
+  }
+  
+  getHours() {
+    return Math.floor(this.getTimes() % (24 * 3600 * 1000) / (3600 * 1000));
+  }
+  
+  getMinutes() {
+    return Math.floor((this.getTimes() % (24 * 3600 * 1000) % (3600 * 1000)) / (60 * 1000));
+  }
+  
+  getSeconds() {
+    return Math.floor((this.getTimes() % (24 * 3600 * 1000) % (3600 * 1000)) % (60 * 1000) / 1000);
+  }
+}
+
+//var ss = (new CaculatorDate("2020/01/04 15:06:45",(new Date()).getTime())).getSeconds();
+
+//计算当前的文章多久前创建的
+$(function () {
+  var m = $(".mulu-time");
+  m.each(function(){
+   var n = $(this).text();
+   var nd = (new CaculatorDate(n,(new Date()).getTime())).getDays();
+   $(this).text(nd+"天前 > ");
+  });
+});
