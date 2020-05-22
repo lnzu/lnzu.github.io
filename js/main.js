070c5ee1d969f112ffd58ff69538f277099348eb
lnzu <2464869577@qq.com>
2020-05-22 11:36:57 +0800

    1.0

--- js/main.js
+++ js/main.js
@@ -39,6 +39,14 @@ class CaculatorDate {
     return this.date2.getTime() - this.date1.getTime();
   }
   
+  getYears() {
+    return Math.floor(this.getTimes() / (365 * 24 * 60 * 60 * 1000));
+  }
+  
+  getMonths() {
+    return Math.floor(this.getTimes() % (365 * 24 * 60 * 60 * 1000) / (30 * 24 * 3600 * 1000));
+  }
+  
   getDays() {
     return Math.floor(this.getTimes() / (24 * 3600 * 1000));
   }
@@ -54,16 +62,34 @@ class CaculatorDate {
   getSeconds() {
     return Math.floor((this.getTimes() % (24 * 3600 * 1000) % (3600 * 1000)) % (60 * 1000) / 1000);
   }
+  
+  getAutoTime() {
+    if (this.getYears() > 0 ) {
+      return this.getYears() + "年";
+    }else if (this.getMonths() >0 ) {
+      return this.getMonths() + "月";
+    }else if (this.getDays() > 0 ) {
+      return this.getDays() + "天";
+    }else if (this.getHours() > 0 ) {
+      return this.getHours() + "小时";
+    }else if (this.getMinutes() > 0) {
+      return this.getMinutes() + "分钟";
+    }else if (this.getSeconds() > 0) {
+      return this.getSeconds() + "秒";
+    }
+  }
 }
 
-//var ss = (new CaculatorDate("2020/01/04 15:06:45",(new Date()).getTime())).getSeconds();
+//var ss = (new CaculatorDate("2020/05/22 09:46:45",(new Date()).getTime()));
+
+//alert(ss.getAutoTime());
 
 //计算当前的文章多久前创建的
 $(function () {
   var m = $(".mulu-time");
   m.each(function(){
    var n = $(this).text();
-   var nd = (new CaculatorDate(n,(new Date()).getTime())).getDays();
-   $(this).text(nd+"天前 > ");
+   var nd = (new CaculatorDate(n,(new Date()).getTime())).getAutoTime();
+   $(this).text(nd+"前 > ");
   });
 });
